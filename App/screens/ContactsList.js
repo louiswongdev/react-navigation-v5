1025546503a7dd490912f056f6ee9cff7e7f5c35
Louis <louis@Louiss-Mac-mini.local>
2020-07-10 16:36:09 -0400

    Commit starter files

---
+++ App/screens/ContactsList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FlatList, SafeAreaView } from 'react-native';
+
+import { Row, Separator } from '../components/Row';
+import users from '../data/users';
+
+export default () => (
+  <SafeAreaView>
+    <FlatList
+      data={users}
+      keyExtractor={item => {
+        return `${item.id.value}-${item.phone}`;
+      }}
+      renderItem={({ item }) => {
+        const name = `${item.name.first} ${item.name.last}`;
+
+        return (
+          <Row
+            image={{ uri: item.picture.thumbnail }}
+            title={name}
+            subtitle={item.email}
+            onPress={() => alert('todo!')}
+          />
+        );
+      }}
+      ItemSeparatorComponent={Separator}
+      ListHeaderComponent={() => <Separator />}
+      ListFooterComponent={() => <Separator />}
+      contentContainerStyle={{ paddingVertical: 20 }}
+    />
+  </SafeAreaView>
+);
